5c9c0e8d479e35241b0ee800fbd11a56beaa2fe2
Cosmin Bota <cosmin.bota29@gmail.com>
2024-11-25 22:44:19 +0200

    Modificari lab 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace BotaCosminLab7
+﻿using System;
+using BotaCosminLab7.Data;
+using System.IO;
+
+namespace BotaCosminLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        { 
+            get 
+            { if (database == null) 
+                { database = new
+                        ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+                }
+                return database; 
+            }
+        }
         public App()
         {
             InitializeComponent();
